01662f2064198a3ffe2c765df0fce01efaacff41
anna.staniszewska <anna.staniszewska93@gmail.com>
2020-09-22 15:22:11 +0200

    Adding day-planner vue project.

---
+++ DayPlanner.UI/day-planner/src/components/DayPlanner.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>DayPlanner</h2>
+        <h3>Today is {{ getCurrentDate() }}</h3>
+    </div>
+</template>
+
+<script>
+import moment from 'moment';
+export default {
+    name: 'Date',
+    moment: moment,
+    methods: {
+        getCurrentDate: function() {
+            return moment().format('DD MMM yyyy');
+        }
+    }
+}
+</script>
+
+<style scoped>
+h3{
+    margin-bottom: 5%;
+}
+</style>
